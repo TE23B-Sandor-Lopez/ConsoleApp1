fcaeba27ceff860cedd14bbcbcf48fa085ffefa5
TE23B-Sandor-Lopez <sandor.lopez@elev.ga.ntig.se>
2024-11-12 13:26:13 +0100

    bvyvyi7

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿
+// if (6 >= 3)
+// {
+//     Console.WriteLine("hello world");
+// }
+// Console.ReadLine();
+
+// Console.WriteLine("Type in username");
+// string name = Console.ReadLine();
+// if  (name == "noname")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+// Console.ReadLine();
+
+// Console.WriteLine("Type in username");
+// string name = Console.ReadLine();
+
+// if  (name == "noname")
+// {
+//     Console.WriteLine("Type in password");
+//     string pass = Console.ReadLine();
+//     if  (pass == "nopass")
+//     {
+//         Console.WriteLine("Welcome");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Try again");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("try again!");
+// }
+// Console.ReadLine();
+
+// for (int hello = 0; hello < 32; hello++)
+// {
+//     Console. WriteLine("Hello, world");
+// }
+// Console.ReadLine(); 
+
+// string lösenord = "";
+// while (lösenord != "Apelsin")
+// {
+//     Console.WriteLine("Testa igen");
+//     lösenord = Console.ReadLine();
+// } 
+// Console.ReadLine();
+
+for (int tal = 0; tal > 5; tal++)
+{
+    Console.WriteLine("Högre än 5!");
+}
